231dc1199d8be41dfbe57a758483fda35f6456e0
Leo Ashkinaziy <github@stonedgecko.org>
2019-08-30 13:01:20 -0700

    Add documentation comments.

--- src/Hangfire.Prometheus/Hangfire.Prometheus/Extensions.cs
+++ src/Hangfire.Prometheus/Hangfire.Prometheus/Extensions.cs
@@ -10,14 +10,17 @@ namespace Hangfire.Prometheus
         /// Initializes Prometheus Hangfire Exporter using current Hangfire job storage and default metrics registry.
         /// </summary>
         /// <param name="app">IApplicationBuilder instance</param>
-        /// <returns></returns>
-        public static IApplicationBuilder UsePrometheusHangfireExporter(this IApplicationBuilder app, HangfirePrometheusSettings settings)
+        /// <returns>Provided instance of IApplicationBuilder</returns>
+        public static IApplicationBuilder UsePrometheusHangfireExporter(this IApplicationBuilder app, HangfirePrometheusSettings settings = null)
         {
+            settings = settings ?? new HangfirePrometheusSettings();
+
             JobStorage js = (JobStorage)app.ApplicationServices.GetService(typeof(JobStorage));
             if (js == null)
             {
                 throw new Exception("Cannot find Hangfire JobStorage class.");
             }
+
             IHangfireMonitorService hangfireMonitor = new HangfireMonitorService(js);
             IPrometheusExporter exporter = new HangfirePrometheusExporter(hangfireMonitor, settings);
             Metrics.DefaultRegistry.AddBeforeCollectCallback(() => exporter.ExportHangfireStatistics());
--- src/Hangfire.Prometheus/Hangfire.Prometheus/HangfirePrometheusSettings.cs
+++ src/Hangfire.Prometheus/Hangfire.Prometheus/HangfirePrometheusSettings.cs
@@ -5,6 +5,10 @@ namespace Hangfire.Prometheus
 {
     public class HangfirePrometheusSettings
     {
+        /// <summary>
+        /// Prometheus collector registry to use. Defaults to Metrics.DefaultRegistry.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Thrown if CollectorRegistry is set to null.</exception>
         public CollectorRegistry CollectorRegistry
         {
             get { return _collectorRegistry; }
@@ -13,9 +17,13 @@ namespace Hangfire.Prometheus
                 _collectorRegistry = value ?? throw new ArgumentNullException();
             }
         }
-
         private CollectorRegistry _collectorRegistry;
 
+        /// <summary>
+        /// Determines behavior when there is an error during Hangfire statistics collection. If set to true,
+        /// ScrapeException is thrown to indicate that scrape should be aborted. If set to false, any error during
+        /// collection is ignored and metric values will not be updated.
+        /// </summary>
         public bool FailScrapeOnException { get; set; }
 
         public HangfirePrometheusSettings()
